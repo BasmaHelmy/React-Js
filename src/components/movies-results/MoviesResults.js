d79641fe1d56439c3a1e1f1335a4798def149c4e
BasmaHelmy <bassmahelmyy@gmail.com>
2018-10-15 20:42:34 +0200

    update

--- src/components/movies-results/MoviesResults.js
+++ src/components/movies-results/MoviesResults.js
@@ -1,1 +1,56 @@
-MoviesResults.js
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { GridList, GridTile } from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import ZoomIn from 'material-ui/svg-icons/action/zoom-in';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
+class MoviesResults extends Component {
+	render() {
+		let showListContent;
+		 const { shows } = this.props;
+		if(shows){
+			showListContent = (
+				<GridList cols={3}>
+					{shows.map(shws => (
+						<GridTile>
+							title={shws.tags}
+							key={shws.id}
+							subtitle={
+								<span>
+								 by<strong>{shws.user}</strong>
+								</span>
+							}
+							actionIcon={
+								<IconButton>
+								  <ZoomIn color="white" />
+								</IconButton>
+
+							}
+							>
+
+							<img src={shws.largeShowURL} />
+
+						</GridTile>
+
+						))}
+				</GridList>
+				)
+		}else{
+		showListContent=null;
+
+		}
+		return (
+          <div>
+ 			{showListContent}
+          </div>
+
+		)
+	}
+}
+MoviesResults.PropTypes ={
+	shows: PropTypes.array.isRequired
+
+}
+export default MoviesResults;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,67 +1,54 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
-import SelectField from 'material-ui/SelectField';
+//import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import axios from 'axios';
+//import MoviesResults from '../movies-results/MoviesResults';
+
 
 class Search extends Component {
 	state={
 		searchText:'',
-		amount:15,
-		apiUrl:'https://www.tvmaze.com/api',
+		apiUrl:'http://api.tvmaze.com/search/shows',
 		apiKey:'896-QWywyNH6JGcZFu5exlRbamIT1meE',
-		movies:[]
+		shows:[]
 	};
 
-	onTextChange = e => {
+	 onTextChange = e => {
     const val = e.target.value;
     this.setState({ [e.target.name]: val }, () => {
       if (val === '') {
-        this.setState({ movies: [] });
+        this.setState({ shows: [] });
       } else {
         axios
           .get(
-            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
-              this.state.searchText
-            }&per_page=${this.state.amount}&safesearch=true`
+            `http://api.tvmaze.com/search/shows?q=${this.searchText}`
           )
-          .then(res => this.setState({ movies: res.data.hits }))
+          .then(res => this.setState({ shows: res.data.show }))
           .catch(err => console.log(err));
       }
     });
   };
 
 
-	render() {
-		console.log(this.state)
-	  return (
-	  	<div>
-  			<TextField
-  				name="searchText"
-  				value={this.state.searchText}
-  				onChange={this.onTextChange}
-  				floatingLabelText="Search For Movies"
-  				fullwidth={true}
-  			/>
-  			<br/>
- 		<SelectField
- 			name="amount"
- 			floatingLabelText="Amount"
-            value={this.state.amount}
-            onChange={this.onAmountChange}
-          >
-           
-            <MenuItem value={5} primaryText="5"/>
-            <MenuItem value={10} primaryText="10"/>
-            <MenuItem value={15} primaryText="15"/>
-            <MenuItem value={30} primaryText="30"/>
-            <MenuItem value={50} primaryText="50"/>
-          </SelectField>
-          <br/>
+	  render() {
+    console.log(this.state.shows);
+    return (
+      <div>
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search For Movies"
+          fullWidth={true}
+        />
+        <br />
+      
+     
 
-	  	</div>
-	  )
-	}
+      </div>
+    );
+  }
+
 }
-
 export default Search;
